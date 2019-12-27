11327543f1556b8bc2716513f5db78dcd3aa5f75
Tamas Soos <soostamas.hu@gmail.com>
2019-12-27 17:11:11 +0100

    Adding `geohash`

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Atlas.MixProject do
   def application do
     [
       mod: {Atlas.Application, []},
-      extra_applications: [:guardian, :logger, :runtime_tools]
+      extra_applications: [:guardian, :geohash, :logger, :runtime_tools,]
     ]
   end
 
@@ -44,7 +44,8 @@ defmodule Atlas.MixProject do
       {:csv, "~> 2.3"},
       {:cors_plug, "~> 2.0"},
       {:pbkdf2_elixir, "~> 1.0"},
-      {:guardian, "~> 2.0"}
+      {:guardian, "~> 2.0"},
+      {:geohash, "~> 1.0"}
     ]
   end
 
